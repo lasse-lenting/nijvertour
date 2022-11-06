c038fe2343e316925fa926eb68bd750fd4b9465b
Lasse Lenting <46818284+lasse-lenting@users.noreply.github.com>
2022-11-06 17:13:47 +0100

    v0.0.3

---
+++ script.js
@@ -0,0 +1,12 @@
+var locaties = {
+    bezoekersCentrum: {
+        locatie: [52.365901, 6.443633],
+        image: "",
+    },
+    sallandseHeuvelRug: {
+        locatie: [52.365901, 6.443633],
+        image: "",
+    },
+}
+
+console.log(locaties.bezoekersCentrum.locatie)
